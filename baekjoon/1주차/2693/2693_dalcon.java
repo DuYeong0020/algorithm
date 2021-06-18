fd87855c217943cf8add9a2aaeeb324babfe813e
yeon <dalcon10280@gmail.com>
2021-06-19 05:30:25 +0900

    [문제풀이] 이연권 2693번 문제풀이

---
+++ baekjoon/1주차/2693/2693_dalcon.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        if (x>0 && y>0) // 1사분면
+            System.out.print(1);
+        else if (x<0 && y>0)// 2사분면
+            System.out.print(2);
+        else if (x<0 && y<0) // 3사분면
+            System.out.print(3);
+        else
+            System.out.print(4);
+    }
+}
